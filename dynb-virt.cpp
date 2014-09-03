26c240cf576853cb9a0006330d8b174d3ce7cbba
Tyler Weimin Ouyang <ouyang@cs.ucsb.edu>
2014-09-02 22:14:53 -0700

    part 1 finished

--- dynb-virt.cpp
+++ dynb-virt.cpp
@@ -23,54 +23,79 @@ public:
 		cout << " }\n";
 	}
 protected:
-	void print_classname() const {
+	virtual void print_classname() const {
 		cout << "Student";
 	}
-	void print_state() const {
+	virtual void print_state() const {
 		cout << "name = " << _name << ", dob = " << _dob;
 	}
 };
 
 class CollegeStudent : public Student {
 private:
-	string _perm;
+    string _perm;
 public:
-	CollegeStudent()
-		: Student(), _perm("") {
-	}
-	CollegeStudent(string name, time_t dob, string perm)
-		: Student(name, dob), _perm(perm) {
-	}
+    CollegeStudent()
+    : Student(), _perm("") {
+    }
+    CollegeStudent(string name, time_t dob, string perm)
+    : Student(name, dob), _perm(perm) {
+    }
 protected:
-	void print_classname() const /*override*/ {
-		cout << "CollegeStudent";
-	}
-	void print_state() const /*override*/ {
-		Student::print_state();
-		cout << ", perm = " << _perm;
-	}
+    void print_classname() const override {
+        cout << "CollegeStudent";
+    }
+    void print_state() const override {
+        Student::print_state();
+        cout << ", perm = " << _perm;
+    }
 };
 
+class CollegeGraduate : public CollegeStudent {
+private:
+    bool _honors;
+public:
+    CollegeGraduate()
+    : CollegeStudent(), _honors(){
+    }
+    CollegeGraduate(string name, time_t dob, string perm, bool honors)
+    : CollegeStudent(name, dob, perm), _honors(honors){
+    }
+protected:
+    void print_classname() const override {
+        cout << "CollegeGraduate";
+    }
+    void print_state() const override {
+        CollegeStudent::print_state();
+        if (_honors == 0)
+            cout << ", honors = no";
+        else
+            cout << ", honors = yes";
+    }
+};
+
+
 int main() {
-	Student john("John Doe", 12311990);
+    Student john("John Doe", 12311990);
 	john.print();
-	cout << "-------------------------\n\n";
-
-	Student &jane = *(new CollegeStudent("Jane Doe", 11211989, "UC-12345"));
-	jane.print();
-	cout << "-------------------------\n\n";
-
-	Student *pstudent = &jane;
-	pstudent->print();
-	cout << "-------------------------\n\n";
-
-	// Student &bilbo = *(new CollegeGraduate("Bilbo Baggins", 2128506,
-    //	"MiddleEarthUniversity-#24123", true));
-    // bilbo.print();
-	// cout << "-------------------------\n\n";
-
-	// delete &bilbo;
-	delete &jane;
-
-	return 0;
+    cout << "-------------------------\n\n";
+    
+    Student &jane = *(new CollegeStudent("Jane Doe", 11211989, "UC-12345"));
+    jane.print();
+    cout << "-------------------------\n\n";
+    
+    
+    Student *pstudent = &jane;
+    pstudent->print();
+    cout << "-------------------------\n\n";
+    
+    Student &bilbo = *(new CollegeGraduate("Bilbo Baggins", 2128506,
+                                           "MiddleEarthUniversity-#24123", true));
+    bilbo.print();
+    cout << "-------------------------\n\n";
+    
+    delete &bilbo;
+    delete &jane;
+    
+    return 0;
 }
--- pa4x/pa4x/main.cpp
+++ pa4x/pa4x/main.cpp
@@ -1,15 +1,48 @@
+////
+////  main.cpp
+////  pa4x
+////
+////  Created by Tyler Weimin Ouyang on 8/27/14.
+////  Copyright (c) 2014 FT: Felicia Truong & Tyler Weimin Ouyang. All rights reserved.
+////
+//#include <iostream>
 //
-//  main.cpp
-//  pa4x
+//using std::cout;
 //
-//  Created by Tyler Weimin Ouyang on 8/27/14.
-//  Copyright (c) 2014 FT: Felicia Truong & Tyler Weimin Ouyang. All rights reserved.
+//// To compile it with "override" keywords present, you
+//// will need a compiler that supports C++11. g++ supports
+//// "override" starting from version 4.7.
 //
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+//class Shape {
+//public:
+//    virtual void draw() { cout << "Shape::draw()\n"; }
+//};
+//
+//class Triangle : public Shape {
+//public:
+//    void draw() override { cout << "Triangle::draw()\n"; }
+//};
+//
+//class Circle : public Shape {
+//public:
+//    void draw() override { cout << "Circle::draw()\n"; }
+//};
+//
+//int main() {
+//    Triangle triangle;
+//    Circle circle;
+//    
+//    Shape shape1(triangle);
+//    shape1.draw();
+//    cout << "-------------------------------\n\n";
+//    
+//    Shape &shape2 = circle;
+//    shape2.draw();
+//    cout << "-------------------------------\n\n";
+//    
+//    Shape *pshape3 = &triangle;
+//    pshape3->draw();
+//    cout << "-------------------------------\n\n";
+//    
+//    return 0;
+//}
